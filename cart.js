96a1742e73bc90fd110ffd78e56ed01e5dd91350
Ruddy <ruddy.pionneau@orange.fr>
2025-09-24 16:09:11 +0200

    delete cart ok

--- cart.js
+++ cart.js
@@ -1,40 +1,65 @@
-fetch("http://localhost:3000/cart", {
-  method: "GET",
-  headers: { "Content-Type": "application/json" },
-})
-  .then((response) => response.json())
-  .then((data) => {
+function getCart() {
+  return fetch("http://localhost:3000/cart", {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  }).then((response) => response.json());
+}
+
+getCart().then(async (data) => {
+  {
     const { cart } = data;
     const { trips } = cart;
-    for (const trip of trips) {
-      console.log(trip);
-      document.querySelector("#cart-container").innerHTML += `
-            <div class="travel">
-            <p class="description">${trip.departure} > ${trip.arrival}</p>
-            <p class="date">${trip.date}</p>
-            <p class="price">${trip.price}€</p>
-            <button class="delete" type="button">X</button>
-          </div>
-            `;
-      updateDeleteTripEventListener(trip);
+    console.log(trips);
+    if (trips.length > 0) {
+      let total = getTotal(trips);
+      document.querySelector("#no-cart").remove();
+      document.querySelector("#cart-container").innerHTML += `<h3>My cart</h3>`;
+      for (const trip of trips) {
+        document.querySelector("#cart-container").innerHTML += `
+              <div class="travel">
+              <p class="description">${trip.departure} > ${trip.arrival}</p>
+              <p class="date">${trip.date}</p>
+              <p class="price">${trip.price}€</p>
+              <button class="delete" type="button">X</button>
+            </div>
+              `;
+      }
+      document.querySelector("#cart-container").innerHTML += ` 
+      <div id="total">
+          <p>Total: ${total}€</p>
+          <button class="purchase-button" type="button" id="purchase">Purchase</button>
+        </div>`;
+      await updateDeleteTripEventListener(trips);
     }
-  });
+  }
+});
 
-function updateDeleteTripEventListener(trip) {
+function getTotal(trips) {
+  let total = 0;
+  for (const trip of trips) {
+    total += trip.price;
+  }
+  return total;
+}
+
+async function updateDeleteTripEventListener(trips) {
   const deleteButtons = document.querySelectorAll(".delete");
+  console.log(deleteButtons);
+  let total = getTotal(trips);
   for (let i = 0; i < deleteButtons.length; i++) {
-    deleteButtons[i].addEventListener("click", function () {
-      fetch(`http://localhost:3000/cart`, {
+    deleteButtons[i].addEventListener("click", async function () {
+      const response = await fetch(`http://localhost:3000/cart`, {
         method: "DELETE",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ tripId: trip._id }),
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          if (data.result) {
-            this.parentNode.remove();
-          }
-        });
+        body: JSON.stringify({ tripId: trips[i]._id }),
+      });
+      const data = await response.json();
+      console.log("data", data);
+      if (data.result) {
+        total -= trips[i].price;
+        deleteButtons[i].parentNode.remove();
+        document.querySelector("#total > p").textContent = total.toString() + "€";
+      }
     });
   }
 }
